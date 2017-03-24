51934eb0426f4248653078adf9ef5064e3c4c2e3
kboddu <kishor.boddu@hotmail.co.uk>
2017-03-24 16:47:04 +0000

    intial commit

---
+++ localmap/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace localmap.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
